add62859e6090767b730c3cd48f4851d319b60e3
erictheericx1 <e.lopez.7.el@gmail.com>
2023-03-19 21:39:45 -0700

    styling

--- models/review.js
+++ models/review.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 const reviewSchema = new mongoose.Schema({
     reviewer: {type: String, required: true},
+    beerType: {type: String, required: true},
     title: {type: String, required: true},
     content: {type: String, required: true},
     rating: {type: Number, min: 0, default: 0}
